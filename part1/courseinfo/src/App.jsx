45cd42ab56ebb87b604fa9f8435c06c02cbc5a14
Andrés Rodríguez Váquez <andres.rod.vaz.77@gmail.com>
2026-03-05 15:04:03 +0200

    2.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,50 @@
-const Content =({}) => {
-  
-}
-
 const Course = ({ course }) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-      {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-    </div>
-  )
-}
+	let total = 0;
+	course.parts.forEach((part) => {
+		total += part.exercises;
+	});
+	return (
+		<div>
+			<h1>{course.name}</h1>
+			{course.parts.map((part) => (
+				<p key={part.id}>
+					{part.name} {part.exercises}
+				</p>
+			))}
+			<h3>total of {total} exercices</h3>
+		</div>
+	);
+};
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },{
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+				id: 3,
+			},
+			{
+				name: "Redux",
+				exercises: 11,
+				id: 4,
+			},
+		],
+	};
 
-  return <Course course={course} />
-}
+	return <Course course={course} />;
+};
 
-export default App
+export default App;
