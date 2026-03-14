aff7d312cecfd8e275a02ea7094a93049823cbd4
Andrés Rodríguez Váquez <andres.rod.vaz.77@gmail.com>
2026-03-14 19:49:23 +0200

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import People from "./components/People";
 
 const App = () => {
-	const [people, setPeople] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [people, setPeople] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [newFilter, setFilter] = useState("");
 
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/persons").then((response) => {
+			console.log("promise fulfilled");
+			setPeople(response.data);
+		});
+	}, []);
 	const handleInputNameChange = (event) => {
 		console.log(event.target.value);
 		setNewName(event.target.value);
